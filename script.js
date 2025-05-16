0985c859d61582443af5ad809c1c578b4734e7e3
Eniitan <enitanolawale0@gmail.com>
2025-05-16 16:54:10 +0100

    refactored the character processing into its own function

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ const response_display = document.getElementById("encryptedText");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   ceaser_cipher(text.value, shift_input.value);
-  console.log(shift.value);
 });
 
-shift_input.addEventListener("input", (e) => {
+shift_input.addEventListener("input", () => {
   shift_input.value = shift_input.value.replace(/[^0-9]/g, "");
   shift_input.value = shift_input.value.slice(0, 1);
 });
@@ -22,30 +21,31 @@ function isLowercase(char) {
   return char === char.toLowerCase() && char !== char.toUpperCase();
 }
 
+function process(char, case_a_ascii_num, shift_num) {
+  const ascii_position = char.charCodeAt(0);
+  const shifted_position =
+    ((ascii_position - case_a_ascii_num + shift_num) % 26) +
+    case_a_ascii_num;
+  const shifted_letter = String.fromCharCode(shifted_position);
+  return shifted_letter;
+}
+
 function ceaser_cipher(text, shift) {
-  let shift_num = Number(shift);
   let encrypted_text = "";
   for (i in text) {
     const char = text[i];
 
     if (isUpperCase(char)) {
-      const ascii_position = char.charCodeAt(0);
-      const shifted_position =
-        ((ascii_position - 65 + shift_num) % 26) + 65;
-      const shifted_letter = String.fromCharCode(shifted_position);
+      const shifted_letter = process(char, 65, Number(shift));
       encrypted_text += shifted_letter;
     } else if (isLowercase(char)) {
-      const ascii_position = char.charCodeAt(0);
-      const shifted_position =
-        ((ascii_position - 97 + shift_num) % 26) + 97;
-      const shifted_letter = String.fromCharCode(shifted_position);
+      const shifted_letter = process(char, 97, Number(shift));
       encrypted_text += shifted_letter;
     } else {
       encrypted_text += char;
     }
   }
 
-  console.log(encrypted_text);
   response_display.innerText = encrypted_text;
   response_display.classList.remove("hidden");
 }
